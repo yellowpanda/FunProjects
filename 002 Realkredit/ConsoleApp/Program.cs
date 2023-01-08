ebc9796ef19871796d313d570ffb9f1cd0c8718b
Rolf Therkildsen <privat@rolft.dk>
2023-01-08 21:46:54 +0100

    Create realkredit project

---
+++ 002 Realkredit/ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Model;
+
+var loan = new MortgageCreditLoan(995658, 12*4, 2, 0.5112M, 25.5M);
+var schedule = loan.GetAmortizationSchedule();
+
+Console.WriteLine("Nummer   Ydelse       Skattefradrag   Renter og afdrag   Afdrag    Ny restgæld");
+
+foreach (var payment in schedule.Payments)
+{
+    Console.WriteLine($"{payment.Number,6} {payment.Installment+payment.Margin,10:0.00}  {payment.TaxReduction,10:0.00} {payment.Margin+payment.Interest,15:0.00} {payment.Installment-payment.Interest,19:0.00} {payment.AmountLeft,10:0.00}" );
+}
+
+Console.WriteLine();
+Console.WriteLine($"Total omkostning:{schedule.Payments.Sum(x => x.Interest + x.Margin),10:0.00}");
+Console.WriteLine($"Total omkostning efter skat:{schedule.Payments.Sum(x => x.Interest + x.Margin - x.TaxReduction),10:0.00}");
+
